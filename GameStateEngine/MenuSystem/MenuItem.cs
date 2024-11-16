b527dba83224eee8babbfe246c67f2dc44876331
ChristopherPH <ChristopherPH@theblackroom.ca>
2024-11-16 14:27:53 -0800

    feat(GameStateEngine): Add GameStateEngine based off of L.T. tag v0.8

---
+++ GameStateEngine/MenuSystem/MenuItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.MenuSystem
+{
+    public class MenuItem
+    {
+        public MenuItem(string Text)
+        {
+            this.Text = Text;
+        }
+
+        public string Text { get; }
+
+        public string SetProperty { get; set; }
+        public object Value { get; set; }
+        public object DoAction { get; set; }
+    }
+}
