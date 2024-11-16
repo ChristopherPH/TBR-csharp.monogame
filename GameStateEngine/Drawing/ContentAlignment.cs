e31c728854ec6e64973df340bd308158b91156c7
ChristopherPH <ChristopherPH@theblackroom.ca>
2024-11-16 14:57:19 -0800

    feat(GameStateEngine): Add some drawing extension methods

---
+++ GameStateEngine/Drawing/ContentAlignment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GameStateEngine.Drawing
+{
+    [Flags]
+    public enum ContentAlignment
+    {
+        TopLeft,
+        TopCenter,
+        TopRight,
+        MiddleLeft,
+        MiddleCenter,
+        MiddleRight,
+        BottomLeft,
+        BottomCenter,
+        BottomRight,
+    }
+}
