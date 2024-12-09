955733763d321ebf78fca0b5710eec84fb4d95d1
ChristopherPH <ChristopherPH@theblackroom.ca>
2024-12-08 16:49:48 -0800

    wip(GuiFramework): Add start of GuiPanel

---
+++ TheBlackRoom.MonoGame.GuiFramework/GuiPanel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace TheBlackRoom.MonoGame.GuiFramework
+{
+    /// <summary>
+    /// Panel Gui Element
+    /// </summary>
+    public class GuiPanel : GuiElement
+    {
+        protected override void DrawGuiElement(GameTime gameTime,
+            ExtendedSpriteBatch spriteBatch, Rectangle drawBounds)
+        {
+
+        }
+    }
+}
