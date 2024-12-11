941f3c32f9d58e62dd147bec9343f60a36fdf676
ChristopherPH <ChristopherPH@theblackroom.ca>
2024-12-10 18:50:02 -0800

    refactor(Drawing): Add comments and rename variables for Rectangle.Shrink()

--- TheBlackRoom.MonoGame/Drawing/RectangleSliceExtensions.cs
+++ TheBlackRoom.MonoGame/Drawing/RectangleSliceExtensions.cs
@@ -4,6 +4,14 @@ namespace TheBlackRoom.MonoGame.Drawing
 {
     public static class RectangleSliceExtensions
     {
+        /// <summary>
+        /// Increases the edges of a rectangle by the given amounts
+        /// </summary>
+        /// <param name="srcRect">Rectangle to inflate</param>
+        /// <param name="leftAmount">Amount to inflate left edge by</param>
+        /// <param name="topAmount">Amount to inflate top edge by</param>
+        /// <param name="rightAmount">Amount to inflate right edge by</param>
+        /// <param name="bottomAmount">Amount to inflate bottom edge by</param>
         public static void Inflate(this ref Rectangle srcRect, int leftAmount,
             int topAmount, int rightAmount, int bottomAmount)
         {
@@ -23,199 +31,245 @@ namespace TheBlackRoom.MonoGame.Drawing
             srcRect.Height += bottomAmount;
         }
 
+        /// <summary>
+        /// Increases all edges of a rectangle by the given amount
+        /// </summary>
+        /// <param name="srcRect">Rectangle to inflate</param>
+        /// <param name="amount">Amount to inflate each edge by</param>
         public static void Inflate(this ref Rectangle srcRect, int amount)
         {
             srcRect.Inflate(amount, amount);
         }
 
+        /// <summary>
+        /// Increases the edges of a rectangle by the given padding
+        /// </summary>
+        /// <param name="srcRect">Rectangle to inflate</param>
+        /// <param name="padding">Amount to inflate each edge by</param>
         public static void Inflate(this ref Rectangle srcRect, Padding padding)
         {
             srcRect.Inflate(padding.Left, padding.Top, padding.Right, padding.Bottom);
         }
 
+        /// <summary>
+        /// Shrinks the edges of a rectangle by the given amounts
+        /// </summary>
+        /// <param name="srcRect">Rectangle to shrink</param>
+        /// <param name="leftAmount">Amount to shrink left edge by</param>
+        /// <param name="topAmount">Amount to shrink top edge by</param>
+        /// <param name="rightAmount">Amount to shrink right edge by</param>
+        /// <param name="bottomAmount">Amount to shrink bottom edge by</param>
         public static void Shrink(this ref Rectangle srcRect, int leftAmount,
             int topAmount, int rightAmount, int bottomAmount)
         {
             srcRect.Inflate(-leftAmount, -topAmount, -rightAmount, -bottomAmount);
         }
 
+        /// <summary>
+        /// Shrinks all edges of a rectangle by the given amount
+        /// </summary>
+        /// <param name="srcRect">Rectangle to shrink</param>
+        /// <param name="amount">Amount to shrink each edge by</param>
         public static void Shrink(this ref Rectangle srcRect, int amount)
         {
             srcRect.Inflate(-amount, -amount);
         }
 
-        public static void Shrink(this ref Rectangle srcRect, int x, int y)
+        /// <summary>
+        /// Shrinks the horizontal and vertical edges by the given amounts
+        /// </summary>
+        /// <param name="srcRect">Rectangle to shrink</param>
+        /// <param name="horizontal">Amount to shrink each horizontal edge by</param>
+        /// <param name="vertical">Amount to shrink each vertical edge by</param>
+        public static void Shrink(this ref Rectangle srcRect, int horizontal, int vertical)
         {
-            srcRect.Inflate(-x, -y);
+            srcRect.Inflate(-horizontal, -vertical);
         }
 
+        /// <summary>
+        /// Shrinks the edges of a rectangle by the given padding
+        /// </summary>
+        /// <param name="srcRect">Rectangle to shrink</param>
+        /// <param name="padding">Amount to shrink rectangle by</param>
         public static void Shrink(this ref Rectangle srcRect, Padding padding)
         {
             srcRect.Inflate(-padding.Left, -padding.Top, -padding.Right, -padding.Bottom);
         }
 
         /// <summary>
-        /// Removes a slice of a rectangle
+        /// Removes a left slice of a rectangle
         /// </summary>
-        /// <param name="Remainder">Leftover rectangle after removing the slice</param>
+        /// <param name="srcRect">Rectangle to slice</param>
+        /// <param name="amount">Amount to slice</param>
+        /// <param name="remainder">Leftover rectangle after removing the slice</param>
         /// <returns>Sliced rectangle</returns>
-        public static Rectangle SliceLeft(this Rectangle SrcRect, int Amount, out Rectangle Remainder)
+        public static Rectangle SliceLeft(this Rectangle srcRect, int amount, out Rectangle remainder)
         {
-            if (SrcRect.IsEmpty || (Amount < 0))
+            if (srcRect.IsEmpty || (amount < 0))
             {
-                Remainder = Rectangle.Empty;
+                remainder = Rectangle.Empty;
                 return Rectangle.Empty;
             }
 
-            if (Amount >= SrcRect.Width)
+            if (amount >= srcRect.Width)
             {
-                Remainder = Rectangle.Empty;
-                return SrcRect;
+                remainder = Rectangle.Empty;
+                return srcRect;
             }
 
             //Rectangles are structs so we can just copy them
-            Remainder = SrcRect;
-            Remainder.X += Amount;
-            Remainder.Width -= Amount;
+            remainder = srcRect;
+            remainder.X += amount;
+            remainder.Width -= amount;
 
-            var rc = SrcRect;
-            rc.Width = Amount;
+            var rc = srcRect;
+            rc.Width = amount;
 
             return rc;
         }
 
         /// <summary>
-        /// Removes a slice of a rectangle
+        /// Removes a left slice of a rectangle
         /// </summary>
-        /// <param name="Percent">0f - 1f</param>
-        /// <param name="Remainder">Leftover rectangle after removing the slice</param>
+        /// <param name="srcRect">Rectangle to slice</param>
+        /// <param name="percent">0f - 1f</param>
+        /// <param name="remainder">Leftover rectangle after removing the slice</param>
         /// <returns>Sliced rectangle</returns>
-        public static Rectangle SliceLeftPercent(this Rectangle SrcRect, float Percent, out Rectangle Remainder)
+        public static Rectangle SliceLeftPercent(this Rectangle srcRect, float percent, out Rectangle remainder)
         {
-            return SrcRect.SliceLeft((int)(SrcRect.Width * Percent), out Remainder);
+            return srcRect.SliceLeft((int)(srcRect.Width * percent), out remainder);
         }
 
         /// <summary>
-        /// Removes a slice of a rectangle
+        /// Removes a right slice of a rectangle
         /// </summary>
-        /// <param name="Remainder">Leftover rectangle after removing the slice</param>
+        /// <param name="srcRect">Rectangle to slice</param>
+        /// <param name="amount">Amount to slice</param>
+        /// <param name="remainder">Leftover rectangle after removing the slice</param>
         /// <returns>Sliced rectangle</returns>
-        public static Rectangle SliceRight(this Rectangle SrcRect, int Amount, out Rectangle Remainder)
+        public static Rectangle SliceRight(this Rectangle srcRect, int amount, out Rectangle remainder)
         {
-            if (SrcRect.IsEmpty || (Amount < 0))
+            if (srcRect.IsEmpty || (amount < 0))
             {
-                Remainder = Rectangle.Empty;
+                remainder = Rectangle.Empty;
                 return Rectangle.Empty;
             }
 
-            if (Amount >= SrcRect.Width)
+            if (amount >= srcRect.Width)
             {
-                Remainder = Rectangle.Empty;
-                return SrcRect;
+                remainder = Rectangle.Empty;
+                return srcRect;
             }
 
             //Rectangles are structs so we can just copy them
-            Remainder = SrcRect;
-            Remainder.Width -= Amount;
+            remainder = srcRect;
+            remainder.Width -= amount;
 
-            var rc = SrcRect;
-            rc.X += SrcRect.Width - Amount;
-            rc.Width = Amount;
+            var rc = srcRect;
+            rc.X += srcRect.Width - amount;
+            rc.Width = amount;
 
             return rc;
         }
 
         /// <summary>
-        /// Removes a slice of a rectangle
+        /// Removes a right slice of a rectangle
         /// </summary>
-        /// <param name="Percent">0f - 1f</param>
-        /// <param name="Remainder">Leftover rectangle after removing the slice</param>
+        /// <param name="srcRect">Rectangle to slice</param>
+        /// <param name="percent">0f - 1f</param>
+        /// <param name="remainder">Leftover rectangle after removing the slice</param>
         /// <returns>Sliced rectangle</returns>
-        public static Rectangle SliceRightPercent(this Rectangle SrcRect, float Percent, out Rectangle Remainder)
+        public static Rectangle SliceRightPercent(this Rectangle srcRect, float percent, out Rectangle remainder)
         {
-            return SrcRect.SliceRight((int)(SrcRect.Width * Percent), out Remainder);
+            return srcRect.SliceRight((int)(srcRect.Width * percent), out remainder);
         }
 
         /// <summary>
-        /// Removes a slice of a rectangle
+        /// Removes a top slice of a rectangle
         /// </summary>
-        /// <param name="Remainder">Leftover rectangle after removing the slice</param>
+        /// <param name="srcRect">Rectangle to slice</param>
+        /// <param name="amount">Amount to slice</param>
+        /// <param name="remainder">Leftover rectangle after removing the slice</param>
         /// <returns>Sliced rectangle</returns>
-        public static Rectangle SliceTop(this Rectangle SrcRect, int Amount, out Rectangle Remainder)
+        public static Rectangle SliceTop(this Rectangle srcRect, int amount, out Rectangle remainder)
         {
-            if (SrcRect.IsEmpty || (Amount < 0))
+            if (srcRect.IsEmpty || (amount < 0))
             {
-                Remainder = Rectangle.Empty;
+                remainder = Rectangle.Empty;
                 return Rectangle.Empty;
             }
 
-            if (Amount >= SrcRect.Height)
+            if (amount >= srcRect.Height)
             {
-                Remainder = Rectangle.Empty;
-                return SrcRect;
+                remainder = Rectangle.Empty;
+                return srcRect;
             }
 
             //Rectangles are structs so we can just copy them
-            Remainder = SrcRect;
-            Remainder.Y += Amount;
-            Remainder.Height -= Amount;
+            remainder = srcRect;
+            remainder.Y += amount;
+            remainder.Height -= amount;
 
-            var rc = SrcRect;
-            rc.Height = Amount;
+            var rc = srcRect;
+            rc.Height = amount;
 
             return rc;
         }
 
         /// <summary>
-        /// Removes a slice of a rectangle
+        /// Removes a top slice of a rectangle
         /// </summary>
-        /// <param name="Percent">0f - 1f</param>
-        /// <param name="Remainder">Leftover rectangle after removing the slice</param>
+        /// <param name="srcRect">Rectangle to slice</param>
+        /// <param name="percent">0f - 1f</param>
+        /// <param name="remainder">Leftover rectangle after removing the slice</param>
         /// <returns>Sliced rectangle</returns>
-        public static Rectangle SliceTopPercent(this Rectangle SrcRect, float Percent, out Rectangle Remainder)
+        public static Rectangle SliceTopPercent(this Rectangle srcRect, float percent, out Rectangle remainder)
         {
-            return SrcRect.SliceTop((int)(SrcRect.Height * Percent), out Remainder);
+            return srcRect.SliceTop((int)(srcRect.Height * percent), out remainder);
         }
 
         /// <summary>
-        /// Removes a slice of a rectangle
+        /// Removes a bottom slice of a rectangle
         /// </summary>
-        /// <param name="Remainder">Leftover rectangle after removing the slice</param>
+        /// <param name="srcRect">Rectangle to slice</param>
+        /// <param name="amount">Amount to slice</param>
+        /// <param name="remainder">Leftover rectangle after removing the slice</param>
         /// <returns>Sliced rectangle</returns>
-        public static Rectangle SliceBottom(this Rectangle SrcRect, int Amount, out Rectangle Remainder)
+        public static Rectangle SliceBottom(this Rectangle srcRect, int amount, out Rectangle remainder)
         {
-            if (SrcRect.IsEmpty || (Amount < 0))
+            if (srcRect.IsEmpty || (amount < 0))
             {
-                Remainder = Rectangle.Empty;
+                remainder = Rectangle.Empty;
                 return Rectangle.Empty;
             }
 
-            if (Amount >= SrcRect.Height)
+            if (amount >= srcRect.Height)
             {
-                Remainder = Rectangle.Empty;
-                return SrcRect;
+                remainder = Rectangle.Empty;
+                return srcRect;
             }
 
             //Rectangles are structs so we can just copy them
-            Remainder = SrcRect;
-            Remainder.Height -= Amount;
+            remainder = srcRect;
+            remainder.Height -= amount;
 
-            var rc = SrcRect;
-            rc.Y += SrcRect.Height - Amount;
-            rc.Height = Amount;
+            var rc = srcRect;
+            rc.Y += srcRect.Height - amount;
+            rc.Height = amount;
 
             return rc;
         }
 
         /// <summary>
-        /// Removes a slice of a rectangle
+        /// Removes a bottom slice of a rectangle
         /// </summary>
-        /// <param name="Percent">0f - 1f</param>
-        /// <param name="Remainder">Leftover rectangle after removing the slice</param>
+        /// <param name="srcRect">Rectangle to slice</param>
+        /// <param name="percent">0f - 1f</param>
+        /// <param name="remainder">Leftover rectangle after removing the slice</param>
         /// <returns>Sliced rectangle</returns>
-        public static Rectangle SliceBottomPercent(this Rectangle SrcRect, float Percent, out Rectangle Remainder)
+        public static Rectangle SliceBottomPercent(this Rectangle srcRect, float percent, out Rectangle remainder)
         {
-            return SrcRect.SliceBottom((int)(SrcRect.Height * Percent), out Remainder);
+            return srcRect.SliceBottom((int)(srcRect.Height * percent), out remainder);
         }
     }
 }
