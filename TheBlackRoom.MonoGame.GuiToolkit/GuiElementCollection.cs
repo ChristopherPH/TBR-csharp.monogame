f43399c2077b26910aca33aeb83b32432262a0ed
ChristopherPH <ChristopherPH@theblackroom.ca>
2025-03-16 14:22:30 -0700

    improve(GuiToolkit): Rework GuiElementCollectionto provide OnAdd/OnRemove overrides

--- TheBlackRoom.MonoGame.GuiToolkit/GuiElementCollection.cs
+++ TheBlackRoom.MonoGame.GuiToolkit/GuiElementCollection.cs
@@ -6,11 +6,43 @@ namespace TheBlackRoom.MonoGame.GuiToolkit
     /// <summary>
     /// Collection of Gui Elements
     /// </summary>
-    public class GuiElementCollection : List<GuiElement>
+    public class GuiElementCollection
     {
+        protected List<GuiElement> Elements { get; } = new List<GuiElement>();
+
+        public void Add(GuiElement element)
+        {
+            if (element == null)
+                return;
+
+            var ix = Elements.Count;
+
+            OnElementAdding(ix, element);
+
+            Elements.Add(element);
+
+            OnElementAdded(ix, element);
+        }
+
+        public void Remove(GuiElement element)
+        {
+            if (element == null)
+                return;
+
+            var ix = Elements.IndexOf(element);
+            if (ix == -1)
+                return;
+
+            OnElementRemoving(ix, element);
+
+            Elements.RemoveAt(ix);
+
+            OnElementRemoved(ix, element);
+        }
+
         public void Update(GameTime gameTime)
         {
-            foreach (GuiElement element in this)
+            foreach (GuiElement element in Elements)
             {
                 element?.Update(gameTime);
             }
@@ -21,7 +53,7 @@ namespace TheBlackRoom.MonoGame.GuiToolkit
             if ((spriteBatch == null) || spriteBatch.IsDisposed)
                 return;
 
-            foreach (GuiElement element in this)
+            foreach (GuiElement element in Elements)
             {
                 element?.Draw(gameTime, spriteBatch);
             }
@@ -29,7 +61,7 @@ namespace TheBlackRoom.MonoGame.GuiToolkit
 
         public GuiElement GetElementAt(Vector2 position)
         {
-            foreach (GuiElement element in this)
+            foreach (GuiElement element in Elements)
                 if (element?.HitTest(position) ?? false)
                     return element;
 
@@ -38,7 +70,7 @@ namespace TheBlackRoom.MonoGame.GuiToolkit
 
         public GuiElement GetElementAt(Point point)
         {
-            foreach (GuiElement element in this)
+            foreach (GuiElement element in Elements)
                 if (element?.HitTest(point) ?? false)
                     return element;
 
@@ -47,11 +79,35 @@ namespace TheBlackRoom.MonoGame.GuiToolkit
 
         public GuiElement GetElementAt(float x, float y)
         {
-            foreach (GuiElement element in this)
+            foreach (GuiElement element in Elements)
                 if (element?.HitTest(x, y) ?? false)
                     return element;
 
             return null;
         }
+
+        /// <summary>
+        /// Occurs when the Gui Element will be added to the
+        /// Gui Element Collection
+        /// </summary>
+        protected virtual void OnElementAdding(int index, GuiElement element) { }
+
+        /// <summary>
+        /// Occurs when the Gui Element has been added to the
+        /// Gui Element Collection
+        /// </summary>
+        protected virtual void OnElementAdded(int index, GuiElement element) { }
+
+        /// <summary>
+        /// Occurs when the Gui Element will be removed from the
+        /// Gui Element Collection
+        /// </summary>
+        protected virtual void OnElementRemoving(int index, GuiElement element) { }
+
+        /// <summary>
+        /// Occurs when the Gui Element has been removed from the
+        /// Gui Element Collection
+        /// </summary>
+        protected virtual void OnElementRemoved(int index, GuiElement element) { }
     }
 }
