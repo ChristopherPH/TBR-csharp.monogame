6824b40be54a91b4dac2303b0cc84d826c0bef03
ChristopherPH <ChristopherPH@theblackroom.ca>
2025-03-18 18:23:10 -0700

    feat(GuiToolkit): Add GuiLayout

---
+++ TheBlackRoom.MonoGame.GuiToolkit/Elements/GuiLayout.cs
@@ -0,0 +1,26 @@
+﻿namespace TheBlackRoom.MonoGame.GuiToolkit.Elements
+{
+    /// <summary>
+    /// Gui Layout, top level element that can contain other Gui Elements
+    /// </summary>
+    public class GuiLayout : GuiElementCollection
+    {
+        /// <summary>
+        /// Adds the specified Gui Element to the layout
+        /// </summary>
+        /// <param name="element">Gui element to add</param>
+        public void Add(GuiElement element)
+        {
+            AddCollectionElement(element);
+        }
+
+        /// <summary>
+        /// Removes the specified Gui Element from the layout
+        /// </summary>
+        /// <param name="element">Gui element to remove</param>
+        public void Remove(GuiElement element)
+        {
+            RemoveCollectionElement(element);
+        }
+    }
+}
