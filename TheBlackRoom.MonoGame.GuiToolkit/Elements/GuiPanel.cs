017331c786d00e86b73c05f0f1d15bb441198fc6
ChristopherPH <ChristopherPH@theblackroom.ca>
2025-03-22 13:39:51 -0700

    feat(GuiToolkit): Allow elements to be added to a panel with an anchor style

--- TheBlackRoom.MonoGame.GuiToolkit/Elements/GuiPanel.cs
+++ TheBlackRoom.MonoGame.GuiToolkit/Elements/GuiPanel.cs
@@ -12,21 +12,24 @@ namespace TheBlackRoom.MonoGame.GuiToolkit.Elements
             = new Dictionary<GuiElement, GuiElementCollectionMetaData>();
 
         /// <summary>
-        /// Adds the specified Gui Element to the panel
+        /// Adds the specified Gui Element to the element collection
         /// </summary>
         /// <param name="element">Gui element to add</param>
-        public void Add(GuiElement element)
+        /// <param name="anchorStyle">Anchor style for gui element</param>
+        public void Add(GuiElement element,
+            GuiElementAnchorStyles anchorStyle = GuiElementAnchorStyles.None)
         {
             if (AddCollectionElement(element))
             {
                 _ElementMetaData[element] = new GuiElementCollectionMetaData
                 {
+                    AnchorStyle = anchorStyle,
                 };
             }
         }
 
         /// <summary>
-        /// Removes the specified Gui Element from the panel
+        /// Removes the specified Gui Element from the element collection
         /// </summary>
         /// <param name="element">Gui element to remove</param>
         public void Remove(GuiElement element)
@@ -34,6 +37,64 @@ namespace TheBlackRoom.MonoGame.GuiToolkit.Elements
             if (RemoveCollectionElement(element))
             {
                 _ElementMetaData.Remove(element);
+            }
+        }
+
+        protected override void OnBoundsChanged(Rectangle oldBounds)
+        {
+            base.OnBoundsChanged(oldBounds);
+
+            //Calculate deltas
+            var dX = Bounds.X - oldBounds.X;
+            var dY = Bounds.Y - oldBounds.Y;
+            var dWidth = Bounds.Width - oldBounds.Width;
+            var dHeight = Bounds.Height - oldBounds.Height;
+
+            //Update element positions and sizes based on anchors
+            foreach (var element in ElementCollection)
+            {
+                if (_ElementMetaData.TryGetValue(element, out var metaData))
+                {
+                    //Get old bounds as a working bounds, to adjust
+                    var bounds = element.Bounds;
+
+                    //If anchored to left and right, then need to adjust position and width,
+                    //otherwise just adjust position
+                    if (metaData.AnchorStyle.HasFlag(GuiElementAnchorStyles.Left) &&
+                        metaData.AnchorStyle.HasFlag(GuiElementAnchorStyles.Right))
+                    {
+                        bounds.X += dX;
+                        bounds.Width += dWidth - dX;
+                    }
+                    else if (metaData.AnchorStyle.HasFlag(GuiElementAnchorStyles.Left))
+                    {
+                        bounds.X += dX;
+                    }
+                    else if (metaData.AnchorStyle.HasFlag(GuiElementAnchorStyles.Right))
+                    {
+                        bounds.X += dWidth;
+                    }
+
+                    //If anchored to top and bottom, then need to adjust position and height,
+                    //otherwise just adjust position
+                    if (metaData.AnchorStyle.HasFlag(GuiElementAnchorStyles.Top) &&
+                        metaData.AnchorStyle.HasFlag(GuiElementAnchorStyles.Bottom))
+                    {
+                        bounds.Y += dY;
+                        bounds.Height += dHeight - dY;
+                    }
+                    else if (metaData.AnchorStyle.HasFlag(GuiElementAnchorStyles.Top))
+                    {
+                        bounds.Y += dY;
+                    }
+                    else if (metaData.AnchorStyle.HasFlag(GuiElementAnchorStyles.Bottom))
+                    {
+                        bounds.Y += dHeight;
+                    }
+
+                    //Set updated bounds
+                    element.Bounds = bounds;
+                }
             }
         }
 
@@ -85,6 +146,7 @@ namespace TheBlackRoom.MonoGame.GuiToolkit.Elements
 
         private class GuiElementCollectionMetaData
         {
+            public GuiElementAnchorStyles AnchorStyle { get; set; }
         }
     }
 }
