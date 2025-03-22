524af10fe9ff761b5f86f6bb53036640c8432260
ChristopherPH <ChristopherPH@theblackroom.ca>
2025-03-22 09:17:48 -0700

    feat(GuiToolkit): Add empty element type

---
+++ TheBlackRoom.MonoGame.GuiToolkit/Elements/GuiEmptyElement.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace TheBlackRoom.MonoGame.GuiToolkit.Elements
+{
+    /// <summary>
+    /// Gui element that does nothing, useful for a hittest,
+    /// only contains base properties
+    /// </summary>
+    public class GuiEmptyElement : GuiElement
+    {
+        protected override void DrawGuiElement(GameTime gameTime,
+            ExtendedSpriteBatch spriteBatch, Rectangle drawBounds)
+        { }
+
+        protected override void UpdateGuiElement(GameTime gameTime) { }
+    }
+}
