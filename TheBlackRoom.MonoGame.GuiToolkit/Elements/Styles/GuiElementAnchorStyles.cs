5483cd18ede185438c9eb85c2488dfaded2a04c4
ChristopherPH <ChristopherPH@theblackroom.ca>
2025-03-22 13:31:06 -0700

    feat(GuiToolkit): Add AnchorStyles

---
+++ TheBlackRoom.MonoGame.GuiToolkit/Elements/Styles/GuiElementAnchorStyles.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+
+namespace TheBlackRoom.MonoGame.GuiToolkit.Elements
+{
+    [Flags]
+    public enum GuiElementAnchorStyles
+    {
+        None = 0,
+
+        Left = 0x01,
+        Top = 0x02,
+        Right = 0x04,
+        Bottom = 0x08,
+
+        TopLeft = Top | Left,
+        TopRight = Top | Right,
+        BottomLeft = Bottom | Left,
+        BottomRight = Bottom | Right,
+
+        All = Left | Top | Right | Bottom
+    }
+
+    public static class GuiElementAnchorStyleExtensions
+    {
+        public static Rectangle ApplyAnchor(this Rectangle elementRect,
+            GuiElementAnchorStyles anchorStyle, Rectangle anchorRect)
+        {
+            if (anchorStyle == GuiElementAnchorStyles.None)
+                return elementRect;
+
+            if (anchorStyle.HasFlag(GuiElementAnchorStyles.Left))
+            {
+                elementRect.X = anchorRect.X;
+            }
+
+            if (anchorStyle.HasFlag(GuiElementAnchorStyles.Right))
+            {
+                elementRect.X = anchorRect.Right - elementRect.Width;
+            }
+
+            return elementRect;
+        }
+    }
+}
