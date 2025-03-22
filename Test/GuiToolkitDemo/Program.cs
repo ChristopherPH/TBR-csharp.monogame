39d04d913c7220d399752621ea6e1607a19692f6
ChristopherPH <ChristopherPH@theblackroom.ca>
2025-03-22 15:22:01 -0700

    wip(Tests): Make it easier to switch between tests

--- Test/GuiToolkitDemo/Program.cs
+++ Test/GuiToolkitDemo/Program.cs
@@ -1,2 +1,21 @@
-﻿using var game = new GuiToolkitDemo.ElementTest();
-game.Run();
+﻿Microsoft.Xna.Framework.Game game = null;
+
+int testNumber = 1;
+
+switch (testNumber)
+{
+    case 1:
+        game = new GuiToolkitDemo.ElementTest();
+        break;
+
+    case 2:
+        game = new GuiToolkitDemo.AnchorTest();
+        break;
+
+    case 3:
+        game = new GuiToolkitDemo.GameGuiTest();
+        break;
+}
+
+game?.Run();
+game?.Dispose();
