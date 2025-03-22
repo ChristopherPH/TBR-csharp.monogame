56c46d2b3f3ec40eebec66fd576501ff57c0b610
ChristopherPH <ChristopherPH@theblackroom.ca>
2025-03-21 21:53:38 -0700

    feat(GuiToolkit): Add GuiPanel.Dock*() methods to help position child controls

--- TheBlackRoom.MonoGame.GuiToolkit/Elements/GuiPanel.cs
+++ TheBlackRoom.MonoGame.GuiToolkit/Elements/GuiPanel.cs
@@ -1,4 +1,6 @@
-﻿namespace TheBlackRoom.MonoGame.GuiToolkit.Elements
+﻿using Microsoft.Xna.Framework;
+
+namespace TheBlackRoom.MonoGame.GuiToolkit.Elements
 {
     /// <summary>
     /// Panel Gui Element
@@ -22,5 +24,51 @@
         {
             RemoveCollectionElement(element);
         }
+
+        /// <summary>
+        /// Returns a rectangle positioned inside the panel,
+        /// with the new rectangles left edge aligned with the
+        /// left edge of the element bounds, and the height of
+        /// the new rectangle equal to the height of the element.
+        /// </summary>
+        /// <param name="Width">Width of rectangle to create</param>
+        /// <returns>Rectangle aligned to left of element</returns>
+        public Rectangle DockLeft(int Width) => new Rectangle(
+                0, 0, MathHelper.Min(MathHelper.Max(0, Width), ContentWidth), ContentHeight);
+
+        /// <summary>
+        /// Returns a rectangle positioned inside the panel,
+        /// with the new rectangles top edge aligned with the
+        /// top edge of the element bounds, and the width of
+        /// the new rectangle equal to the width of the element.
+        /// </summary>
+        /// <param name="Height">Height of rectangle to create</param>
+        /// <returns>Rectangle aligned to top of element</returns>
+        public Rectangle DockTop(int Height) => new Rectangle(
+                0, 0, ContentWidth, MathHelper.Min(MathHelper.Max(0, Height), ContentHeight));
+
+        /// <summary>
+        /// Returns a rectangle positioned inside the panel,
+        /// with the new rectangles right edge aligned with the
+        /// right edge of the element bounds, and the height of
+        /// the new rectangle equal to the height of the element.
+        /// </summary>
+        /// <param name="Width">Width of rectangle to create</param>
+        /// <returns>Rectangle aligned to left of element</returns>
+        public Rectangle DockRight(int Width) => new Rectangle(
+                ContentWidth - MathHelper.Min(MathHelper.Max(0, Width), ContentWidth), 0,
+                MathHelper.Min(MathHelper.Max(0, Width), ContentWidth), ContentHeight);
+
+        /// <summary>
+        /// Returns a rectangle positioned inside the panel,
+        /// with the new rectangles bottom edge aligned with the
+        /// bottom edge of the element bounds, and the width of
+        /// the new rectangle equal to the width of the element.
+        /// </summary>
+        /// <param name="Height">Height of rectangle to create</param>
+        /// <returns>Rectangle aligned to bottom of element</returns>
+        public Rectangle DockBottom(int Height) => new Rectangle(
+                0, ContentHeight - MathHelper.Min(MathHelper.Max(0, Height), ContentHeight),
+                ContentWidth, MathHelper.Min(MathHelper.Max(0, Height), ContentHeight));
     }
 }
